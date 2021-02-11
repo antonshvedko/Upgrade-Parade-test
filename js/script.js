1806ddd84b4ddbd34aba1ca8cd0137406d11ede9
antonshvedko <antonshvedko996@gmail.com>
2021-02-11 12:52:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('.menu-opener');
+
+if (typeof menu !== 'undefined') {
+    menu.addEventListener('click', (event) => {
+        event.preventDefault();
+        let header = document.querySelector('.header');
+        if (typeof header !== 'undefined') {
+            header.classList.toggle('mobile-menu-active');
+        }
+    });
+}
+
+
+
+
+
